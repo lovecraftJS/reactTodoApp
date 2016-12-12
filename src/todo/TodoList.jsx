6403e8fb5690d0cae7bd7388fd6f896847073f0c
lovecraftJS <lovecraftjs@ya.ru>
2016-12-13 01:51:10 +0300

    created template

---
+++ src/todo/TodoList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <Todo />
+    );
+  }
+}
+
+export default TodoList;
---
+++ src/todo/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default TodoApp;
